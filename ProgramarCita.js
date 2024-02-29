23260e242963f6f56e90150c7236d56e9fd067ff
Kori0867 <156251964+Kori0867@users.noreply.github.com>
2024-02-28 20:23:22 -0600

    Update ProgramarCita.js
    ACTUALIZADO

--- ProgramarCita.js
+++ ProgramarCita.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ProgramarCita() {
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Lógica para manejar la programación de la cita
+    // FORMULARIO CITAS
   };
 
   return (
@@ -14,7 +14,7 @@ function ProgramarCita() {
           <label htmlFor="nombre" className="form-label">Nombre:</label>
           <input type="text" id="nombre" name="nombre" required className="form-control"/>
         </div>
-        {/* Otros campos del formulario de cita */}
+      
         <button type="submit" className="btn btn-primary">Programar Cita</button>
       </form>
     </div>
